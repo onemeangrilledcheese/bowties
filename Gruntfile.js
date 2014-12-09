b1296e5cfdc7cc2843cd2fef14cabfd6ce61dd23
Chris B <gossblackmon@gmail.com>
2014-12-09 01:49:21 -0600

    Initial commit. Styled navbar and items, ready to style off-set menu.

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    copy: {
+      scripts: {
+        expand: true,
+        cwd: 'bower_components/',
+        src: '**/*.js',
+        dest: 'js'
+      },
+
+      maps: {
+        expand: true,
+        cwd: 'bower_components/',
+        src: '**/*.map',
+        dest: 'js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'js/modernizr/modernizr.min.js': ['js/modernizr/modernizr.js']
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+          'js/foundation/js/foundation.min.js',
+          'js/custom/*.js'
+        ],
+
+        dest: 'js/app.js'
+      }
+
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      },
+      options: {
+        livereload: true,
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', ['sass', 'copy', 'uglify', 'concat']);
+  grunt.registerTask('default', ['watch']);
+}
